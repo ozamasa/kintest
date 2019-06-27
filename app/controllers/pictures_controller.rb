b6f1c7c1d7f883d1883e5c3858972b47a58efa99
Masaki Ozawa <ozamasa@shiolab.com>
2019-06-27 18:54:04 +0900

    update pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -3,7 +3,22 @@ class PicturesController < ApplicationController
   def index
     require 'kintone'
     
-    api = Kintone::Api.new("example.cybozu.com", "authtoken")
+    api = Kintone::Api.new(ENV["KINTONE_HOST"], ENV["CYBOZU_API_TOKEN"])
+    app = ENV["KINTONE_APP"]
+    id = ENV["KINTONE_ID"]
+
+    fields = ["name", "file", "作成日時"]
+    query = Kintone::Query.new { f("作成日時") == today }  #limit 5
+
+    @files = []
+    api.records.get(app, query, fields)["records"].each do |record|
+      value = record["file"]["value"].first
+      h = {}
+      h[:file] = api.file.get(record["file"]["value"].first["fileKey"])
+      h[:name] = record["name"]["value"]
+      h[:created_at] = record["作成日時"]["value"]
+      @files << h
+    end
   end
 
 end
