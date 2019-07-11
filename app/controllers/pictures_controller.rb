469e384c9d44505e94cdb2b442e6bfc3bdcbb39c
Masaki Ozawa <ozamasa@shiolab.com>
2019-07-11 17:34:46 +0900

    update layout

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -6,18 +6,25 @@ class PicturesController < ApplicationController
     api = Kintone::Api.new(ENV["KINTONE_HOST"], ENV["CYBOZU_API_TOKEN"])
     app = ENV["KINTONE_APP"]
 
-    file = ENV["KINTONE_FIELD_FILE"]
     title = ENV["KINTONE_FIELD_TITLE"]
     created_at = ENV["KINTONE_FIELD_CREATED"]
 
-    query = Kintone::Query.new { f(created_at) == today }  #limit 5
-    fields = [ file, title, created_at ]
+    file1 = ENV["KINTONE_FIELD_FILE1"]
+    file2 = ENV["KINTONE_FIELD_FILE2"]
+    file3 = ENV["KINTONE_FIELD_FILE3"]
+
+    query = Kintone::Query.new { limit 5 } # { f(created_at) == today }
+    fields = [ file1, file2, file3, title, created_at ].uniq
 
     @files = []
     api.records.get(app, query, fields)["records"].each do |record|
       @files << Hash[
-        file:       (api.file.get(record[file]["value"].first["fileKey"]) rescue nil),
-        type:       (record[file]["value"].first["contentType"] rescue nil),
+        file1:      (api.file.get(record[file1]["value"].first["fileKey"]) rescue nil),
+        file2:      (api.file.get(record[file2]["value"].first["fileKey"]) rescue nil),
+        file3:      (api.file.get(record[file3]["value"].first["fileKey"]) rescue nil),
+        type1:      (record[file1]["value"].first["contentType"] rescue nil),
+        type2:      (record[file2]["value"].first["contentType"] rescue nil),
+        type3:      (record[file3]["value"].first["contentType"] rescue nil),
         title:      (record[title]["value"] rescue nil),
         created_at: (record[created_at]["value"] rescue nil)
       ]
